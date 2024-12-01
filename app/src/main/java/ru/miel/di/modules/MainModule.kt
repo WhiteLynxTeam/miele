a5ed61206b4eda2d06378392c4d1553cfd19617f
Yuris <yuris.musin@mail.ru>
2024-12-01 23:15:47 +0500

    добавил фрагмент авторизации

--- app/src/main/java/ru/miel/di/modules/MainModule.kt
+++ app/src/main/java/ru/miel/di/modules/MainModule.kt
@@ -3,9 +3,13 @@ package ru.miel.di.modules
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import ru.miel.view.activity.MainActivity
+import ru.miel.view.auth.AuthFragment
 
 @Module
 interface MainModule {
     @ContributesAndroidInjector
     fun bindMainActivity(): MainActivity
+
+    @ContributesAndroidInjector
+    fun bindAuthFragment(): AuthFragment
 }
