b1e11c65029999263e35234c4ecc67bd09ec9b87
Mea <maevski.ed@gmail.com>
2024-12-26 14:36:41 +0300

    подправил библиотеки toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,14 +81,11 @@ dependencies {
     implementation(libs.retrofit.adapters.result)
 
     /** Glide */
-    implementation("com.github.bumptech.glide:glide:4.15.1")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
-
-    /** SnapHelper - выравнивание элементов recyclerview */
-    implementation("com.github.rubensousa:gravitysnaphelper:2.2.2")
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
 
     /** ROOM */
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    kapt(libs.room.compiler)
 }
